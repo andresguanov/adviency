fc61aa3045c8e040c5093e4feddee3a2acb45c61
andresguanov <andresguanov@gmail.com>
2021-12-18 09:30:18 -0500

    Complete Day 18

---
+++ day-18/src/constants/index.js
@@ -0,0 +1,9 @@
+export const constants = {
+    URLdefault: 'https://static.vecteezy.com/system/resources/previews/002/205/948/non_2x/gift-box-icon-free-vector.jpg',
+
+    randomGifts: ['PlayStation', 'Switch', 'IPhone12', 'Xbox', 'Ropa', 'Pantalones', 'Television', 'Laptop']
+
+
+}
+
+
